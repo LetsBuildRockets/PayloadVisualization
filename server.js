6613746abb65991fdec79238232e74041886bb65
Eric Sims <EricSimsM@gmail.com>
2015-09-12 23:00:32 -0400

    ready for maker faire

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort(process.argv[2], {
+  baudrate: 115200
+});
+
+serialPort.on("open", function () {
+  console.log('open');
+  var cmd = '';
+  serialPort.on('data', function(data) {
+    cmd = cmd.concat(data.toString());
+    if(cmd.indexOf('\n', cmd.length - 1) !== -1) {
+      try {
+        var obj = JSON.parse(cmd);
+        console.log(obj);
+        io.sockets.emit('data', obj);
+      } catch (e) {
+        console.log(cmd);
+      }
+      cmd = '';
+    }
+  });
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
 });
